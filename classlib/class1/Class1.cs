b087797a5f649fb417370734de97f74c99fe41f3
Student <foobar@sr.htlweiz.at>
2026-04-13 11:17:17 +0200

    Step 3: first Figures

--- classlib/class1/Class1.cs
+++ classlib/class1/Class1.cs
@@ -1,23 +1,36 @@
-﻿namespace ChessGameClass;
+﻿using System.Runtime.CompilerServices;
+
+namespace ChessGameClass;
 
 public class ChessFigure
 {
-
 }
 
 public class Chess
 {
 
-    // =================== SPIELFELD ==================
+    public char[,] board = new char[8, 8];
+
+    public Chess() {
+
+        for(int i = 0; i < 8; i++) {
+            
+            board[1, i] = 'b';
+            board[6, i] = 'B';
+        }
+
+
+    }
+
+
+    // ############### SPIELFELD ###############
     public override string ToString()
     {
-
         var sb = new System.Text.StringBuilder();
         int size = 8;
 
         string line = "+";
         for (int i = 0; i < size; i++)
-            // somit kann dynamisch die Größe des Feldes gesetzt werden
             line += "--+";
 
         for (int row = 0; row < size; row++)
@@ -28,11 +41,20 @@ public class Chess
             {
                 sb.Append("|");
 
-                if ((row + col) % 2 == 0) // jedes zweite Feld soll weiß sein
+                char Figur = board[row, col];
 
+                if(Figur != '\0') {
+                    
+                    sb.Append(Figur + " ");
+                }
+                else if ((row + col) % 2 == 0)
+                {
                     sb.Append("##"); // weißes Feld
+                }
                 else
+                {
                     sb.Append("  "); // schwarzes Feld
+                }
             }
 
             sb.AppendLine("|");
@@ -42,12 +64,5 @@ public class Chess
 
         return sb.ToString();
     }
-
-    // ================================================
-
-
-
-    
+    // #########################################
 }
-
-
--- classlib/class1/obj/Debug/net10.0/class1.AssemblyInfo.cs
+++ classlib/class1/obj/Debug/net10.0/class1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("class1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c466df748ba7adb637d7d48beadba61ae97759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65b5677cf00115242e42be54a8d3344661b4506")]
 [assembly: System.Reflection.AssemblyProductAttribute("class1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("class1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
