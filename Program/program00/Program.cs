410eb18d858790923899250ded29dd3df33d9836
Student <foobar@sr.htlweiz.at>
2026-04-28 22:42:02 +0200

    Step 5: first Movement

--- Program/program00/Program.cs
+++ Program/program00/Program.cs
@@ -1,15 +1,25 @@
 ﻿using ChessGameClass;
 
-namespace ChessGameProgram {
+namespace ChessGameProgram
+{
 
-    public class Program {
+    public class Program
+    {
 
-        public static void Main() {
-
+        public static void Main()
+        {
 
             Chess board = new Chess();
 
             Console.WriteLine();
+            board.Draw();
+
+             // (y, x) 
+            
+            board.MoveBauer(1, 1, 2, 1); // 1, 0 in dem Fall Startposition
+
+            Console.WriteLine("\nNach Zug:\n");
+
             board.Draw();
 
         }
--- classlib/class1/Class1.cs
+++ classlib/class1/Class1.cs
@@ -25,7 +25,7 @@ public class Chess
         board[0, 0] = board[0, 7] = '♖';
         board[7, 0] = board[7, 7] = '♜';
 
-        
+
         // Pferde
 
         board[0, 1] = board[0, 6] = '♘';
@@ -42,53 +42,72 @@ public class Chess
 
         board[0, 4] = '♕';
         board[7, 4] = '♛';
-        
+
 
         // König
 
         board[0, 3] = '♔';
         board[7, 3] = '♚';
 
-
-
-
     }
-
 
     // ############### SPIELFELD ###############
     public void Draw()
-{
-    int size = 8;
-
-    for (int row = 0; row < size; row++)
     {
-        for (int col = 0; col < size; col++)
+        int size = 8;
+
+        for (int row = 0; row < size; row++)
         {
-            bool isWhite = (row + col) % 2 == 0;
-
-            // Hintergrund setzen
-            Console.BackgroundColor = isWhite
-                ? ConsoleColor.White
-                : ConsoleColor.DarkGray;
-
-            char figure = board[row, col];
-
-            if (figure != '\0')
+            for (int col = 0; col < size; col++)
             {
+                bool isWhite = (row + col) % 2 == 0;
 
-                Console.Write($"{figure} ");
-            }
-            else
-            {
-                Console.Write("  ");
+                Console.BackgroundColor = isWhite
+                    ? ConsoleColor.White
+                    : ConsoleColor.DarkGray;
+
+                char figure = board[row, col];
+
+                if (figure != '\0')
+                {
+
+                    Console.Write($"{figure} ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+
+                Console.ResetColor();
             }
 
-            // Reset nach jedem Feld ist optional, aber sauber
-            Console.ResetColor();
+            Console.WriteLine();
         }
-
-        Console.WriteLine();
     }
+
+    // ############### BAUERN-BEWEGUNG ###############
+
+    public bool MoveBauer(int fromRow, int fromCol, int toRow, int toCol)
+{
+    char pawn = board[fromRow, fromCol];
+
+    // Test ob Figur Bauer ist
+    if (pawn != '♙' && pawn != '♟')
+        return false;
+
+    // Richtung festlegen (darf sich nur 1 Feld bewegen)
+    int direction = pawn == '♙' ? 1 : -1;
+
+    if (toCol == fromCol && toRow == fromRow + direction)
+    {
+        if (board[toRow, toCol] == '\0')
+        {
+            board[toRow, toCol] = pawn;
+            board[fromRow, fromCol] = '\0';
+            return true;
+        }
+    }
+
+    return false;
 }
-    // #########################################
 }
